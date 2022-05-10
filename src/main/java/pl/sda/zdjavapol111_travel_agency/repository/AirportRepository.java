b1c13bccdc232a26fad3b52671954b90238f3005
Monika <adamczyk.monikaa@gmail.com>
2022-05-10 21:03:23 +0200

    repository templates have been modified

--- src/main/java/pl/sda/zdjavapol111_travel_agency/repository/AirportRepository.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/repository/AirportRepository.java
@@ -1,7 +1,8 @@
 package pl.sda.zdjavapol111_travel_agency.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.sda.zdjavapol111_travel_agency.model.Airport;
 
-public class AirportRepository extends JpaRepository<Airport, Integer> {
+public interface AirportRepository extends JpaRepository<Airport, Integer> {
     Airport findById(Integer id);
 }
--- src/main/java/pl/sda/zdjavapol111_travel_agency/repository/RoleRepository.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/repository/RoleRepository.java
@@ -1,5 +1,8 @@
 package pl.sda.zdjavapol111_travel_agency.repository;
 
-public class RoleRepository extends JpaRepository<Role, Integer> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.sda.zdjavapol111_travel_agency.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
     Role findByName(String name);
 }
