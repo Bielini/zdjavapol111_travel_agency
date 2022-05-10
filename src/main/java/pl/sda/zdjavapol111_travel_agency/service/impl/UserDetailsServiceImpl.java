15f457fccc4da9f9a461d91ef2dae583b9dc6e5c
kisielw <weronika.bednarek1@gmail.com>
2022-05-10 20:33:51 +0200

    Security configuration - fixed

--- src/main/java/pl/sda/zdjavapol111_travel_agency/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/service/impl/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package pl.sda.zdjavapol111_travel_agency.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,6 +9,8 @@ import java.util.stream.Collectors;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
