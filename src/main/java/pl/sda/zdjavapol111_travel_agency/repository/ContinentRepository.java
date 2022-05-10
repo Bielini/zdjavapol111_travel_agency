ac932f1e6fc07272eee6a07b628e012c5e69e493
Monika <adamczyk.monikaa@gmail.com>
2022-05-10 21:03:23 +0200

    repository templates have been modified

--- src/main/java/pl/sda/zdjavapol111_travel_agency/repository/ContinentRepository.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/repository/ContinentRepository.java
@@ -3,6 +3,6 @@ package pl.sda.zdjavapol111_travel_agency.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.sda.zdjavapol111_travel_agency.model.Continent;
 
-public class ContinentRepository extends JpaRepository <Continent, Integer>{
+public interface ContinentRepository extends JpaRepository <Continent, Integer>{
     Continent findById(Integer id);
 }
