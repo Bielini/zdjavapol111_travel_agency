694b3ebc6b70206841010cb508c988bc27f1bdb2
Piotr B <piotr.bielawski@onet.pl>
2022-05-10 18:34:27 +0200

    h2 database connection established

--- src/main/java/pl/sda/zdjavapol111_travel_agency/Zdjavapol111TravelAgencyApplication.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/Zdjavapol111TravelAgencyApplication.java
@@ -1,3 +1,5 @@
+package pl.sda.zdjavapol111_travel_agency;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/security/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package pl.sda.zdjavapol111_travel_agency.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .headers().frameOptions().disable();
+    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+//
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+//        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+//
+//        auth.authenticationProvider(daoAuthenticationProvider);
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+//        return  new BCryptPasswordEncoder();
+//    }
+//}
+}
