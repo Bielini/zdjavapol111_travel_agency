a34c5758f23fe798a766a59e7632721dffb3ee15
Monika <adamczyk.monikaa@gmail.com>
2022-05-11 17:43:33 +0200

    repository templates - removed methods

--- src/main/java/pl/sda/zdjavapol111_travel_agency/repository/ContinentRepository.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/repository/ContinentRepository.java
@@ -1,8 +1,8 @@
 package pl.sda.zdjavapol111_travel_agency.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sda.zdjavapol111_travel_agency.model.Continent;
+
 
 public interface ContinentRepository extends JpaRepository <Continent, Integer>{
-    Continent findById(Integer id);
+
 }
--- src/main/java/pl/sda/zdjavapol111_travel_agency/repository/TourRepository.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/repository/TourRepository.java
@@ -1,8 +1,8 @@
 package pl.sda.zdjavapol111_travel_agency.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sda.zdjavapol111_travel_agency.model.Tour;
+
 
 public interface TourRepository<Travel> extends JpaRepository <Tour, Long> {
-    Tour findById(Integer id);
+
 }
