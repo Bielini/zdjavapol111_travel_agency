d7fe87146eeae0db22d8a201f4287060c310a72e
Piotr B <piotr.bielawski@onet.pl>
2022-05-11 20:39:00 +0200

    undo changes

--- src/main/java/pl/sda/zdjavapol111_travel_agency/repository/TourRepository.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/repository/TourRepository.java
@@ -3,6 +3,5 @@ package pl.sda.zdjavapol111_travel_agency.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface TourRepository<Travel> extends JpaRepository <Tour, Long> {
+public interface TourRepository<Travel> extends JpaRepository<Tour, Long> {
 
-}
