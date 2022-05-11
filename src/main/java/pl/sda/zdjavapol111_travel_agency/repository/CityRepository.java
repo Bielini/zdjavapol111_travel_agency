12d67229ccd59368b7a799dee1ed4555badc1c27
Monika <adamczyk.monikaa@gmail.com>
2022-05-11 17:43:33 +0200

    repository templates - removed methods

--- src/main/java/pl/sda/zdjavapol111_travel_agency/repository/CityRepository.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/repository/CityRepository.java
@@ -1,9 +1,8 @@
 package pl.sda.zdjavapol111_travel_agency.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sda.zdjavapol111_travel_agency.model.City;
+
 
 public interface CityRepository extends JpaRepository<City, Integer> {
-    City findById(Integer id);
-    City findByName(String name);
+
 }
