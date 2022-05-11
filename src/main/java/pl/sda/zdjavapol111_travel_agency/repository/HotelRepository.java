0ddc73294c17b38870251db2a7cf4a99a5599fc2
Monika <adamczyk.monikaa@gmail.com>
2022-05-11 18:11:00 +0200

    repository templates - removed methods

--- src/main/java/pl/sda/zdjavapol111_travel_agency/repository/HotelRepository.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/repository/HotelRepository.java
@@ -1,6 +1,7 @@
 package pl.sda.zdjavapol111_travel_agency.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.sda.zdjavapol111_travel_agency.model.Hotel;
 
 
 public interface HotelRepository extends JpaRepository <Hotel, Integer> {
