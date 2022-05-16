9a0ebdb6d731615ac72eb7596c22107015b4dd32
kisielw <weronika.bednarek1@gmail.com>
2022-05-16 21:55:30 +0200

    cosmetics changes in TourController (use only services)

--- src/main/java/pl/sda/zdjavapol111_travel_agency/controller/TourController.java
+++ src/main/java/pl/sda/zdjavapol111_travel_agency/controller/TourController.java
@@ -1,16 +1,28 @@
 package pl.sda.zdjavapol111_travel_agency.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.sda.zdjavapol111_travel_agency.model.Tour;
 import pl.sda.zdjavapol111_travel_agency.repository.CityRepository;
 import pl.sda.zdjavapol111_travel_agency.repository.TourRepository;
 import pl.sda.zdjavapol111_travel_agency.service.CityService;
 import pl.sda.zdjavapol111_travel_agency.service.TourService;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.sda.zdjavapol111_travel_agency.model.Tour;
+import pl.sda.zdjavapol111_travel_agency.service.TourService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -19,19 +31,14 @@ public class TourController {
 
     private TourService tourService;
 
-    private CityService cityService;
-
     private List<Tour> filteredTours;
 
-    private String activeFilter = "";
-    private CityRepository cityRepository;
+    private CityService cityService;
 
 
-
-    public TourController(TourService tourService, CityRepository cityRepository, TourRepository tourRepository, CityService cityService) {
+    public TourController(TourService tourService, CityService cityService) {
         this.tourService = tourService;
         this.filteredTours = tourService.getAllTours();
-        this.cityRepository = cityRepository;
         this.cityService = cityService;
     }
 
@@ -40,7 +47,6 @@ public class TourController {
         modelMap.addAttribute("promotedTours", tourService.get2PromotedTours());
         modelMap.addAttribute("tours", tourService.getAllTours());
         modelMap.addAttribute("filteredTours", filteredTours);
-        modelMap.addAttribute("activeFilter", activeFilter);
 
         return "tour-list";
     }
@@ -49,7 +55,6 @@ public class TourController {
     public String handleNewUserByAdmin(@RequestParam("filter") String filter, @RequestParam("searchField") String searchField) {
         log.info("Received filter: " + filter + " & value of field: " + searchField);
         this.filteredTours = tourService.filterTours(searchField, filter);
-        this.activeFilter = tourService.getActiveFilter(searchField,filter);
         return "redirect:/tours";
     }
 
@@ -57,58 +62,17 @@ public class TourController {
     @GetMapping(path = "/admin/add-tour")
     String showCreateTourForm(ModelMap modelMap) {
         modelMap.addAttribute("emptyTour", new Tour());
-        modelMap.addAttribute("cities", cityRepository.findAll());
+        modelMap.addAttribute("cities", cityService.findAll());
         return "tour-create";
     }
 
-    @PostMapping(path = "/admin/save")
+    @PostMapping (path = "/admin/save")
     String handleNewTour(@ModelAttribute("emptyTour") Tour tour) {
         log.info("Handled new tour: " + tour);
         tourService.calculateDuration(tour);
         tourService.save(tour);
+
         return "redirect:/admin/panel";
     }
 
-    @GetMapping(path = "/tours/{id}")
-    public String showTourDescription(@PathVariable Integer id, ModelMap modelMap) {
-        Tour tour = tourService.getById(id);
-        modelMap.addAttribute("tour", tour);
-        return "tour-description";
-    }
-
-    @GetMapping(path = "/admin/tours/{id}/prom")
-    public String flipPromTour(@PathVariable Integer id){
-        Tour tour = tourService.getById(id);
-        tourService.updatePromById(id,!tour.getPromotion());
-        log.info("Received: " + id +" ,and change tour to promoted: "+ !tour.getPromotion());
-        return "redirect:/tours";
-    }
-    @GetMapping(path = "/admin/tours/{id}/delete")
-    public String deleteTour(@PathVariable Integer id){
-        tourService.deleteById(id);
-        log.info("Deleted tour of id: " + id);
-        filteredTours=tourService.getAllTours();
-        activeFilter = "";
-        return "redirect:/tours";
-    }
-
-    @GetMapping(path = "/admin/tours/{id}/edit")
-    public String showEditTourForm(@PathVariable Integer id,ModelMap modelMap){
-        modelMap.addAttribute("oldTour",tourService.getById(id));
-        modelMap.addAttribute("newTour", new Tour());
-        modelMap.addAttribute("cities", cityService.findAll());
-
-        return "tour-edit";
-    }
-
-    @PostMapping(path = "/admin/tours/{id}/update")
-    public String handleUpdatedTour(
-            @PathVariable() Integer id,
-            @ModelAttribute(name = "originCity") String originCity,
-            @ModelAttribute(name = "adultSeats") Integer adultSeats){
-
-        log.info(id+originCity+adultSeats);
-
-        return "redirect:/admin/tours/{id}/edit";
-    }
 }
